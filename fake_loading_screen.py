0d03a23a12a50b7290a262806da0480a923b4994
Hunter Burke <151594415+Hunter-Burke@users.noreply.github.com>
2024-05-17 15:34:17 -0600

    fake loading screen

---
+++ fake_loading_screen.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+def loading_text():
+  sleep(1.5)
+  print("\nLoading", end = "")
+  for i in range(4):
+    sleep(1)
+    if i < 3:
+      print(".", end = "")
+    else:
+      print("")
+  sleep(2)
